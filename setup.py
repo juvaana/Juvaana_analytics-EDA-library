9df64b5a9d9b83262e4705ffffde585e5a68b63f
kennedy benard <kennedybenard73@gmail.com>
2026-05-19 11:40:59 +0300

    Version control

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="juvaana-analytics",
-    version="0.1.0",
+    version="0.1.1",
     author="Juvaana Team",
     description="Interactive EDA + Visualization + HTML reporting library",
     packages=find_packages(),
